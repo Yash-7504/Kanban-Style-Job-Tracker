ad19cb53f61270a7b98f81c117e7e0e787628ac4
Yash <k.yashvenkat14@yahoo.com>
2025-11-02 09:47:09 +0000

    bug fixes

--- client/components/JobModal.tsx
+++ client/components/JobModal.tsx
@@ -137,6 +137,7 @@ export default function JobModal({ isOpen, onClose, onSave, job }: JobModalProps
               value={formData.dateApplied}
               onChange={(e) => setFormData({ ...formData, dateApplied: e.target.value })}
               className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+              style={{ boxSizing: 'border-box', maxWidth: '100%' }}
             />
           </div>
           
